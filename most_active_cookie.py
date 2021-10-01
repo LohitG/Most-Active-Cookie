7b0d8e80cecb9e7196badad8aecce4c8ad3960f9
Lohit Geddam <lohitgeddam@Lohits-MBP.lan>
2021-09-30 23:21:28 -0700

    take in arguments and add test file

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import argparse
 
-parser = argparse.ArgumentParser()
-
+parser = argparse.ArgumentParser(description = 'Process arguments')
+parser.add_argument('filename')
+parser.add_argument('-d')
+args = parser.parse_args()
 def test_function(test):
 	return test
 
-print(test_function(3))
+print(args.d)

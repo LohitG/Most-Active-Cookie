7637c30bc310fce3196c1e3cf6a4215d1642efc0
Lohit Geddam <lohitgeddam@Lohits-MBP.lan>
2021-10-01 00:01:25 -0700

    finished and tested using given values/tests

--- most_active_cookie.py
+++ most_active_cookie.py
@@ -1,11 +1,45 @@
 #!/usr/bin/env python
 import argparse
 
+# parse through arguments and store them in args as an iterable
 parser = argparse.ArgumentParser(description = 'Process arguments')
 parser.add_argument('filename')
 parser.add_argument('-d')
 args = parser.parse_args()
-def test_function(test):
-	return test
 
-print(args.d)
+# open file
+cookie_file = open(args.filename, "r")
+
+# skip first line
+cookie_file.readline()
+
+dict_of_dates = {}
+
+for line in cookie_file:
+	components = line.split(',')
+	components[1] = components[1][:10]
+	if components[1] in dict_of_dates:
+		dict_of_dates[components[1]] += [components[0]]
+	else:
+		dict_of_dates[components[1]] = [components[0]]
+
+cookies_from_date = dict_of_dates[args.d]
+
+def find_mode(lst):
+	dict_of_cookies = {}
+	for cookie in lst:
+		if cookie in dict_of_cookies:
+			dict_of_cookies[cookie] += 1
+		else:
+			dict_of_cookies[cookie] = 1
+	mode_values = []
+	max_frequency = max(dict_of_cookies.values())
+	for cookie, frequency in dict_of_cookies.items():
+		if frequency == max_frequency:
+			mode_values += [cookie]
+	return mode_values
+
+list_of_modes = find_mode(cookies_from_date)
+
+for cookie in list_of_modes:
+	print(cookie)
